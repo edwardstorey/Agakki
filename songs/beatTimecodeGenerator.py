45b784dbbfded02474a7ce72e6c2b20b441bb836
Garcia Lu <gargar070402@gmail.com>
2020-02-29 07:10:01 -0500

    Created YMCA beatmap Garcia's portion

--- songs/beatTimecodeGenerator.py
+++ songs/beatTimecodeGenerator.py
@@ -2,11 +2,11 @@
 
 # Between partialBeatTimecodeList[i] and partialBeatTimecodeList[i + 1],
 # there are durationList[i] beats
-partialBeatTimecodeList = [1.212, 5.034, 8.848,
-    12.592, 16.406, 20.162, 23.899,
+partialBeatTimecodeList = [1.212, 5.034, 8.848, # intro
+    12.592, 16.406, 20.162, 23.899, # verse 1
     27.603, 31.379, 35.220, 38.991,
-    42.674,
-    46.417, 50.200, 53.959, 57,727,
+    42.674, #bridge
+    46.417, 50.200, 53.959, 57,727, # chorus
     61.459, 65.215, 68.990, 72.737,
     76.440]
 durationList = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8] # duration list has one less item than beatTimecodeList
