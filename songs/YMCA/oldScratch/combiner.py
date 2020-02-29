3bbf49f280c771cd4dfab13f8980bb776a44b563
Garcia Lu <gargar070402@gmail.com>
2020-02-29 11:47:07 -0500

    Updated beatmap combiner

--- songs/YMCA/oldScratch/combiner.py
+++ songs/YMCA/oldScratch/combiner.py
@@ -1,23 +1,29 @@
-import re
 import csv
 
 beatList = []
 mapList = []
-with open("../YMCABeats.txt",'r') as f:
+csvReader = None
+with open("YMCA-Beats.txt",'r') as f:
     beatList = f.readlines()
-with open("YMCA-Beatmap-final.csv",'r') as f:
-    mapList = f.readlines()
-with open("YMCA-Beatmap.csv",'w') as f:
-    f.write(mapList[0])
-    for s in mapList[1:]:
-        beatNumber = float(re.search(r'(.+?),',s).group(1))
-        time = float(beatList[int(beatNumber)-1])
-        if(beatNumber!=int(beatNumber)):
-            time+=float(beatList[int(beatNumber)])
-            time/=2
-        f.write(str(time)+re.search(r'(,.+)',s).group(1)+"\n")
-        
-    #= beatList[int(re.search(r'(.+?),',mapList[1]).group(1))]-1
+beatList.insert(0, 0) # insert dummy value in front to make beatList[i] equal to time value at beat i
+csvReader = csv.DictReader(open("YMCA-Beatmap-final.csv",'r'))
 
-#print(beatList[int(re.search(r'(.+?),',mapList[1]).group(1))]-1)
-#output = open("YMCA-Beatmap.csv",'w')
+with open("YMCA-Beatmap-test.csv",'w') as f:
+    
+    f.write("time,left,right,both,comments\n")
+
+    for row in csvReader:
+        beatNumber = float(row['beatNo'])
+        time = float(beatList[int(beatNumber)])
+
+        if (beatNumber != int(beatNumber)):
+            # Assumes there is always an integer beat after current beat value
+            time += (float(beatList[int(beatNumber) + 1]) - float(beatList[int(beatNumber)])) * (beatNumber - float(int(beatNumber)))
+
+        # outputs new line to f
+        f.write(str(time))
+        for key in row.keys():
+            if (key != 'beatNo'):
+                f.write(',' + str(row[key]))
+        f.write('\n')
+
