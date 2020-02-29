e31a66ff279a1fef8cf0fad92fe3137562b5a543
Garcia Lu <gargar070402@gmail.com>
2020-02-29 05:51:13 -0500

    Updated YMCA timecodes

--- songs/beatTimecodeGenerator.py
+++ songs/beatTimecodeGenerator.py
@@ -1,12 +1,25 @@
 # To be cleaned up
 
-firstBeat = 1.212
-duration = 75.229
-beatsInDuration = 160
-secondsPerBeat = duration / beatsInDuration
+# Between partialBeatTimecodeList[i] and partialBeatTimecodeList[i + 1],
+# there are durationList[i] beats
+partialBeatTimecodeList = [1.212, 5.034, 8.848,
+    12.592, 16.406, 20.162, 23.899,
+    27.603, 31.379, 35.220, 38.991,
+    42.674,
+    46.417, 50.200, 53.959, 57,727,
+    61.459, 65.215, 68.990, 72.737,
+    76.440]
+durationList = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8] # duration list has one less item than beatTimecodeList
+secondsPerBeatList = []
+beatTimecodeList = []
+for i in range(len(durationList)):
+    secondsPerBeatList.append((partialBeatTimecodeList[i + 1] - partialBeatTimecodeList[i]) / durationList[i])
+    for j in range(durationList[i]):
+        beatTimecodeList.append(partialBeatTimecodeList[i] + (secondsPerBeatList[i] * j))
+beatTimecodeList.append(partialBeatTimecodeList[-1])
 
-print(secondsPerBeat)
+print(secondsPerBeatList)
 
 with open('YMCABeats.txt', 'w') as output:
-	for i in range(160):
-		output.write(str(firstBeat + secondsPerBeat * i) + '\n')
+    for timecode in beatTimecodeList:
+        output.write(str(timecode) + '\n')
