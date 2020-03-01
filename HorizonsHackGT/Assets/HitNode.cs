1cfecffeaab3dfc96adb12edd3927b1482ffb819
Edward Storey <edwardstorey01@gmail.com>
2020-02-29 19:31:42 -0500

    Initial Commit
    Initial commit for Unity project and associated files.

---
+++ HorizonsHackGT/Assets/HitNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitNode
+{
+    public float time;
+    public int left;
+    public int right;
+    public int both;
+
+    public HitNode(float time, int left, int right, int both)
+    {
+        this.time = time;
+        this.left = left;
+        this.right = right;
+        this.both = both;
+    }
+}
